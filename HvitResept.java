e1709f8feee45bc9c9d592b03f0f9807bfb00405
anastaan <62110923+anastaan@users.noreply.github.com>
2020-03-13 10:59:10 +0100

    Add files via upload
    Oppdateringer i forbindelse med Pasient

--- HvitResept.java
+++ HvitResept.java
@@ -1,10 +1,12 @@
 public class HvitResept extends Resept{
 
-    public HvitResept(Legemiddel legemiddel, Lege utskrivendeLege, int pasientId, int reit){
-        super(legemiddel, utskrivendeLege, pasientId, reit);
+//torsdag 12.03 - Anastasia endret kons. til å ta inn Pasient i stedet for pasientId. Endret på denne delen av toString.
+
+    public HvitResept(Legemiddel legemiddel, Lege utskrivendeLege, Pasient pasient, int reit){
+        super(legemiddel, utskrivendeLege, pasient, reit);
     }
 
-    
+
     public double prisAaBetale(){
         return this.hentLegemiddel().hentPris();
         //fullpris
@@ -16,9 +18,9 @@ public class HvitResept extends Resept{
     }
 
     @Override
-    public String toString(){//overriding the toString() method  
-        return "Hvit resept på: \n" + this.hentLegemiddel() + "\nUtskrevet av: " 
-            + this.hentLege().hentNavn() + "\nTil pasienten med ID: " + 
+    public String toString(){//overriding the toString() method
+        return "Hvit resept på: \n" + this.hentLegemiddel() + "\nUtskrevet av: "
+            + this.hentLege().hentNavn() + "\nTil pasienten med ID: " +
             this.hentPasientId() + "\nAntall gjenværende iterasjoner: " + this.hentReit() + "\nPris å betale: " + this.prisAaBetale() + " nok";
-    } 
+    }
 }
--- Militarresept.java
+++ Militarresept.java
@@ -1,7 +1,9 @@
 public class Militarresept extends HvitResept{
 
-    public Militarresept(Legemiddel legemiddel, Lege utskrivendeLege, int pasientId, int reit){
-        super(legemiddel, utskrivendeLege, pasientId, reit);
+//torsdag 12.03 - Anastasia endret kons. til å ta inn Pasient i stedet for pasientId. Endret på denne delen av toString.
+
+    public Militarresept(Legemiddel legemiddel, Lege utskrivendeLege, Pasient pasient, int reit){
+        super(legemiddel, utskrivendeLege, pasient, reit);
     }
 
     @Override
@@ -10,10 +12,10 @@ public class Militarresept extends HvitResept{
     }
 
     @Override
-    public String toString(){//overriding the toString() method  
-        return "Hvit militærresept på: \n" + this.hentLegemiddel() + "\nUtskrevet av: " 
-            + this.hentLege().hentNavn() + "\nTil pasienten med ID: " + 
+    public String toString(){//overriding the toString() method
+        return "Hvit militærresept på: \n" + this.hentLegemiddel() + "\nUtskrevet av: "
+            + this.hentLege().hentNavn() + "\nTil pasienten med ID: " +
             this.hentPasientId() + "\nAntall gjenværende iterasjoner: " + this.hentReit() + "\nPris å betale: " + this.prisAaBetale() + " nok";
-    } 
-    
+    }
+
 }
