2e88a3a622ee74624f30a354e7b16b8a34593077
Mickkkkkkk <62139773+Mickkkkkkk@users.noreply.github.com>
2020-03-14 14:31:26 +0100

    Update Resept.java

--- Resept.java
+++ Resept.java
@@ -1,61 +1,55 @@
 public abstract class Resept{
-/*
-torsdag 12.03 - Anastasia endret kons. til å ta inn Pasient i stedet for pasientId.
-                Endret derfor på metoden hentPasientId og denne delen av toString.
-*/
 
-    //deklarerer instansevariabler
     public static int idTeller = 0;
     private int id;
     private Legemiddel legemiddel;
     protected Lege utskrivendeLege;
-    private Pasient pasient; //endret pasientId til Pasient pasient
-    private int reit; //reiterasjoner
+    private Pasient pasient;
+    private int reit;
 
-
-    public Resept(Legemiddel legemiddel, Lege utskrivendeLege, Pasient pasient, int reit){
-        idTeller++;
-        id = idTeller;
-        this.legemiddel = legemiddel;
-        this.utskrivendeLege = utskrivendeLege;
-        this.pasient = pasient;
-        this.reit = reit;
-
+    public Resept(Legemiddel legemiddel, Lege utskrivendeLege, Pasient pasient, int reit) throws NullPointerException{
+      if (legemiddel == null || utskrivendeLege == null || pasient == null) throw new NullPointerException();
+      
+      id = idTeller;
+      idTeller++;
+  
+      this.legemiddel = legemiddel;
+      this.utskrivendeLege = utskrivendeLege;
+      this.pasient = pasient;
+      this.reit = reit;
     }
 
     public int hentId(){
-        return id;
+      return id;
     }
 
     public Legemiddel hentLegemiddel(){
-        return legemiddel;
+      return legemiddel;
     }
 
     public Lege hentLege(){
-        return utskrivendeLege;
+      return utskrivendeLege;
     }
 
     public int hentPasientId(){
-        return pasient.hentId();
+      return pasient.hentId();
     }
 
     public int hentReit(){
-        return reit;
+      return reit;
     }
 
     public boolean bruk(){
-        if (reit > 0){
-            reit --;
-            return true;
-        } else {
-            return false;
-        }
-    } //det sto ikke noe om at reit skulle minke men jeg antok
+      if (reit > 0){
+          reit --;
+          return true;
+      } 
+      return false;
+    }
 
     public abstract String farge();
 
     public abstract double prisAaBetale();
 
     public abstract String toString();
-
 }
